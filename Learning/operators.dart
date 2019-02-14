8f944dad311b5999021280dd556d32abda1b1c59
AnishaSayone <anisha.sayone@gmail.com>
2019-02-14 18:02:37 +0530

    operators

---
+++ Learning/operators.dart
@@ -0,0 +1,20 @@
+Add(int a, int b){
+print(a+b);
+}
+Sub(int c, int d){
+print(c-d);
+}
+Mul(int e,int f){
+  print(e*f);
+}
+Div(int g, int h){
+  print(g/h);
+}
+main(){
+  
+  Add(2,3);
+  Sub(90, 45);
+  Mul(11, 33);
+  Div(80, 8);
+
+}
